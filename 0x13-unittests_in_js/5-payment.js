7f7a4358d9d529abeae16b66dd9c2b66a9a292d2
tomas.estrada <tomas.estrada@rappi.com>
2022-01-05 13:12:04 -0500

    adding unnitest for js

---
+++ 0x13-unittests_in_js/5-payment.js
@@ -0,0 +1,15 @@
+const utils = require('./utils');
+
+module.exports = function sendPaymentRequestToApi(totalAmount, totalShipping) {
+    const total = utils.calculateNumber('SUM', totalAmount, totalShipping);
+    console.log(`The total is: ${total}`);
+    return total;
+};
+
+const utils = require('./utils');
+
+module.exports = function sendPaymentRequestToApi(totalAmount, totalShipping) {
+    const total = utils.calculateNumber('SUM', totalAmount, totalShipping);
+    console.log(`The total is: ${total}`);
+    return total;
+};
---
+++ 0x13-unittests_in_js/6-payment_token.js
@@ -0,0 +1,9 @@
+module.exports = function getPaymentTokenFromAPI(success) {
+    if (success === true)
+        return Promise.resolve({data: 'Successful response from the API'});
+};
+
+module.exports = function getPaymentTokenFromAPI(success) {
+    if (success === true)
+        return Promise.resolve({data: 'Successful response from the API'});
+};
